263707b7d5d11d21de364f458a07d70d66236f81
HASSANALIGOHAR <49777223+HASSANALIGOHAR@users.noreply.github.com>
2019-04-21 09:51:47 -0700

    Add files via upload

---
+++ q7.py
@@ -0,0 +1,8 @@
+#Question No 7
+#Calculate The Factorial
+import math
+num=0
+while(num!="-"):
+  num=int(input("Enter value:::"))
+  factorial= math.factorial(num)
+  print("The factorial is" , num , "is" , factorial)
