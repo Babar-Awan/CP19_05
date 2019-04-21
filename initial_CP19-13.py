b682d9420905e2eee8eedc71285e0293927208b6
Babar-Awan <49777147+Babar-Awan@users.noreply.github.com>
2019-04-21 22:08:13 +0500

    This is our initial file that I have uploaded

---
+++ initial_CP19-13.py
@@ -0,0 +1,10 @@
+print ("We 5 students :M. Babar Awan , Farooq Arman , Haziq , Arsalan , Hassan . Are trying to make a face detection application (13).")
+a= input ("Enter any sort of number : ")
+if (a>13):
+	print ("you have entered wrong number")
+else:
+	print ("Lets start working on this PROJECT ....")
+
+print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print ("|      		THE                            END                |")
+print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
